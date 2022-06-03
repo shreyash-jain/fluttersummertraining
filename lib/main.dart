3b25f0c518ff527e89931fbdda42052bc282bcea
shreyash-jain <shreyash.jain.eee15@itbhu.ac.in>
2022-06-04 00:09:57 +0530

    changing from stateless to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Toggle(),
+      home: ToggleWorking(),
     );
   }
 }
@@ -42,6 +42,54 @@ class Toggle extends StatelessWidget {
               ElevatedButton(
                 child: Text("Black2"),
                 onPressed: () {
+                  print(6);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ToggleWorking extends StatefulWidget {
+  ToggleWorking({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => ToggleWorkingState();
+}
+
+class ToggleWorkingState extends State<ToggleWorking> {
+  Color color = Colors.black;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: color,
+      body: SafeArea(
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: Text("Red"),
+                onPressed: () {
+                  print(5);
+                  setState(() {
+                    color = Colors.red;
+                  });
+                  print(color);
+                },
+              ),
+              SizedBox(width: 20),
+              ElevatedButton(
+                child: Text("Black"),
+                onPressed: () {
+                  setState(() {
+                    color = Colors.black;
+                  });
+
                   print(6);
                 },
               ),
