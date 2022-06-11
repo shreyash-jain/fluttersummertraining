091812309b6ea8d419678a07d433fb4ca64c316e
shreyash-jain <shreyash.jain.eee15@itbhu.ac.in>
2022-06-11 09:52:26 +0530

    added basic navigator

--- lib/main.dart
+++ lib/main.dart
@@ -11,119 +11,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ToggleWorking(),
+      home: FirstRoute(),
     );
   }
 }
 
-class Toggle extends StatelessWidget {
-  Toggle({Key? key}) : super(key: key);
-  Color color = Colors.black;
+class FirstRoute extends StatelessWidget {
+  const FirstRoute();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: color,
-      body: SafeArea(
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                child: Text("Red"),
-                onPressed: () {
-                  print(5);
-                  color = Colors.red;
-                  print(color);
-                },
-              ),
-              SizedBox(width: 20),
-              ElevatedButton(
-                child: Text("Black"),
-                onPressed: () {
-                  print(6);
-                },
-              ),
-            ],
-          ),
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Page 1'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
         ),
       ),
     );
   }
 }
 
-class ToggleWorking extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return ToggleWorkingState();
-  }
-}
-
-class ToggleWorkingState extends State<ToggleWorking> {
-  Color color = Colors.white;
-  int counterValue = 0;
-  TextEditingController nameController = TextEditingController(text: "no data");
-
-  getEmail() {
-    return "email@gmail.com";
-  }
-
-  @override
-  void initState() {
-    
-    nameController.text = getEmail();
-    // do some computaion
-    super.initState();
-  }
+class SecondRoute extends StatelessWidget {
+  const SecondRoute();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: color,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                child: Text("Add"),
-                onPressed: () {
-                  setState(() {});
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ThirdRoute()),
+            );
+          },
+          child: const Text('Page 2'),
+        ),
+      ),
+    );
+  }
+}
 
-                  counterValue = counterValue + 1;
 
-                  print(counterValue);
-                },
-              ),
-              SizedBox(width: 20),
-              Text(counterValue.toString()),
-              SizedBox(width: 20),
-              ElevatedButton(
-                child: Text("Sub"),
-                onPressed: () {
-                  counterValue = counterValue - 1;
-                  setState(() {});
-                  print(counterValue);
-                },
-              ),
-              SizedBox(width: 20),
-              Text("Please type your name"),
-              SizedBox(width: 20),
-              TextField(
-                controller: nameController,
-              ),
-              if (counterValue < 10)
-                ElevatedButton(
-                  child: Text("I am here"),
-                  onPressed: () {
-                    setState(() {});
-                    print(nameController.text);
-                  },
-                ),
-            ],
-          ),
+class ThirdRoute extends StatelessWidget {
+  const ThirdRoute();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.popUntil(
+                context, (Route<dynamic> route) => route.isFirst);
+          },
+          child: const Text('Page 3'),
         ),
       ),
     );
